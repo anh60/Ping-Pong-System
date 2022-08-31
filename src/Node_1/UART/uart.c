b3eddb8d118d41e3bf2b678ede64ca08e83f2298
Daniel Guarecuco <daniel.guarecuco@gmail.com>
2022-08-31 17:15:11 +0200

    Working UART TX -- RX to be tested

---
+++ src/Node_1/UART/uart.c
@@ -0,0 +1,38 @@
+#include <avr/io.h>
+#include "uart.h"
+
+void USART_Init(unsigned int ubrr)
+{
+    /* Set baud rate */
+    UBRRH = (unsigned char)(ubrr >> 8);
+    UBRRL = (unsigned char)ubrr;
+    /* Enable receiver and transmitter */
+    UCSRB = (1 << RXEN) | (1 << TXEN);
+    /* Set frame format: 8data, 2stop bit */
+    UCSRC = (1 << URSEL) | (1 << USBS) | (3 << UCSZ0);
+}
+
+void USART_Transmit(unsigned char data)
+{
+    /* Wait for empty transmit buffer */
+    while (!(UCSRA & (1 << UDRE)))
+        ;
+    /* Put data into buffer, sends the data */
+    UDR = data;
+}
+
+unsigned char USART_Receive(void)
+{
+    /* Wait for data to be received */
+    while (!(UCSRA & (1 << RXC)))
+        ;
+    /* Get and return received data from buffer */
+    return UDR;
+}
+
+void USART_Flush(void)
+{
+    unsigned char dummy;
+    while (UCSRA & (1 << RXC))
+        dummy = UDR;
+}
