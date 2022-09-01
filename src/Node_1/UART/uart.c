98a18786598f36b409986e3842af5a961d0d870c
Marcus Alexander Tjomsaas s299571 <s299571@studenti.polito.it>
2022-09-01 14:27:14 +0200

    CHnaged register names

--- src/Node_1/UART/uart.c
+++ src/Node_1/UART/uart.c
@@ -4,35 +4,35 @@
 void USART_Init(unsigned int ubrr)
 {
     /* Set baud rate */
-    UBRRH = (unsigned char)(ubrr >> 8);
-    UBRRL = (unsigned char)ubrr;
+    UBRR0H = (unsigned char)(ubrr >> 8);
+    UBRR0L = (unsigned char)ubrr;
     /* Enable receiver and transmitter */
-    UCSRB = (1 << RXEN) | (1 << TXEN);
+    UCSR0B = (1 << RXEN0) | (1 << TXEN0);
     /* Set frame format: 8data, 2stop bit */
-    UCSRC = (1 << URSEL) | (1 << USBS) | (3 << UCSZ0);
+    UCSR0C = (1 << URSEL0) | (1 << USBS0) | (3 << UCSZ00);
 }
 
 void USART_Transmit(unsigned char data)
 {
     /* Wait for empty transmit buffer */
-    while (!(UCSRA & (1 << UDRE)))
+    while (!(UCSR0A & (1 << UDRE0)))
         ;
     /* Put data into buffer, sends the data */
-    UDR = data;
+    UDR0 = data;
 }
 
 unsigned char USART_Receive(void)
 {
     /* Wait for data to be received */
-    while (!(UCSRA & (1 << RXC)))
+    while (!(UCSR0A & (1 << RXC0)))
         ;
     /* Get and return received data from buffer */
-    return UDR;
+    return UDR0;
 }
 
 void USART_Flush(void)
 {
     unsigned char dummy;
-    while (UCSRA & (1 << RXC))
-        dummy = UDR;
+    while (UCSR0A & (1 << RXC0))
+        dummy = UDR0;
 }
