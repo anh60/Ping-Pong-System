ec8675f8b62317f4ac5f77b3ef185afc1996b3a9
Daniel Guarecuco <daniel.guarecuco@gmail.com>
2022-11-17 21:33:36 +0100

    Fixing comments from previous commit

--- src/Node_1/Atmega162/Atmega162/XMEM/xmem.c
+++ src/Node_1/Atmega162/Atmega162/XMEM/xmem.c
@@ -21,11 +21,15 @@ void xmem_init(void){
 	SFIOR |= (1 << XMM2 ); // mask unused bits (PC7-PC4 used for JTAG)
 	SFIOR &= ~(1 << XMM1 );
 	SFIOR &= ~(1 << XMM0 );
-}// Write to external SRAM
+}
+
+// Write to external SRAM
 void xmem_write(uint8_t data , uint16_t addr){
 	volatile char *ext_mem = (char *) BASE_ADDRESS;
 	ext_mem[addr]= data;
-}// Read from external SRAM
+}
+
+// Read from external SRAM
 uint8_t xmem_read(uint16_t addr){
 	volatile char *ext_mem = (char *) BASE_ADDRESS;
 	uint8_t ret_val = ext_mem[addr];
