7a3c82dca1b34e33e9c9f7d08ab500a2df2d9f25
Daniel Guarecuco <daniel.guarecuco@gmail.com>
2022-11-17 21:30:05 +0100

    Adding comments

--- src/Node_1/Atmega162/Atmega162/SPI/spi.c
+++ src/Node_1/Atmega162/Atmega162/SPI/spi.c
@@ -8,16 +8,16 @@
  *	Marcus Tjomsaas
  */
 
-#include "spi.h"/* Init procedure. Partially taken from datasheet */void SPI_init(void){
+#include "spi.h"/* Init procedure. Partially taken from datasheet */void SPI_init(void){
 	/* Set SSn, MOSI and SCK as output, all others input */
 	DDRB = (1<<DDB4)|(1<<DDB5)|(1<<DDB7);
 	/* Enable SPI, Master, set clock rate fck/16 */
-	SPCR = (1<<SPE)|(1<<MSTR)|(1<<SPR0);	/* Set SPI Mode = 0 (CPOL=0, CPHA=0) */
-	SPCR &= ~(1<<CPOL)| ~(1<<CPHA);}
+	SPCR = (1<<SPE)|(1<<MSTR)|(1<<SPR0);	/* Set SPI Mode = 0 (CPOL=0, CPHA=0) */
+	SPCR &= ~(1<<CPOL)| ~(1<<CPHA);}
 void SPI_send(uint8_t data){
 	/* Transmission starts when writing to SPDR register */
 	SPDR = data;
 	/* Wait for transmission complete */
 	while(!(SPSR & (1<<SPIF)))
-	;}
-uint8_t SPI_read(void){	SPI_send(0xAA); //Send dummy data	//Read data left in the SPI data register	return SPDR;}
+	;}
+uint8_t SPI_read(void){	SPI_send(0xAA); //Send dummy data	//Read data left in the SPI data register	return SPDR;}
